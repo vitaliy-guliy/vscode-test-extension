276cef75c716baab39865c0248c0ffbf98633013
Vitaliy Gulyy <vgulyy@redhat.com>
2024-09-14 17:54:11 +0300

    Test commit
    Signed-off-by: Vitaliy Gulyy <vgulyy@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -12,61 +12,10 @@ import * as vscode from 'vscode';
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
     context.subscriptions.push(vscode.commands.registerCommand('test.test-input', async () => {
-        const port = await enterExposedPort();
-
-        const exposure = await enterExposure();
-
-        await vscode.window.showInformationMessage(`Exposed port ${port}:${exposure}`);
+        await vscode.window.showInformationMessage('Test message');
 
     }));
 
-}
-
-async function enterExposedPort(): Promise<number | undefined> {
-    const port = await vscode.window.showInputBox({
-        value: '8080',
-        title: 'Exposed Port'
-    });
-
-    return Number.parseInt(port);
-}
-
-async function enterExposure(): Promise<'public' | 'internal' | 'none' | undefined> {
-    const dPublic = 'Endpoint will be exposed on the public network';
-    const dInternal = 'Endpoint will be exposed internally outside of the main devworkspace POD';
-    const dNone = 'Endpoint will not be exposed and will only be accessible inside the main devworkspace POD';
-
-    const items: vscode.QuickPickItem[] = [
-        {
-            label: 'public',
-            detail: dPublic
-        },
-        {
-            label: 'internal',
-            detail: dInternal
-        },
-        {
-            label: 'none',
-            detail: dNone
-        }
-    ];
-
-    const item = await vscode.window.showQuickPick(items, {
-        title: 'Describe how the port should be exposed on the network'
-    });
-
-    if (item) {
-        switch (item.label) {
-            case 'public':
-                return 'public';
-            case 'internal':
-                return 'internal';
-            case 'none':
-                return 'none';
-        }
-    }
-
-    return undefined;
 }
 
 // This method is called when your extension is deactivated
